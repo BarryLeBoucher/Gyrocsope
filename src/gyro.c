97915becd4ce23066d28e0683dc7f68c3b33c236
BarryLeBoucher <cyril.moty@hotmail.fr>
2021-04-05 21:42:00 +0200

    Modification dans gyro.c

--- src/gyro.c
+++ src/gyro.c
@@ -7,7 +7,7 @@ do\n\
     PATH_X=$(cat /sys/bus/iio/devices/iio\:device1/in_anglvel_x_raw) \n\
     PATH_Y=$(cat /sys/bus/iio/devices/iio\:device1/in_anglvel_y_raw) \n\
     PATH_Z=$(cat /sys/bus/iio/devices/iio\:device1/in_anglvel_z_raw) \n\
-    echo "x = $PATH_X : y = $PATH_Y : z = $PATH_Z" >>/home/gyro.txt \n\
+    echo \"x = $PATH_X : y = $PATH_Y : z = $PATH_Z\" >>/home/gyro.txt \n\
     sleep 1 \n\
 done \n\"
  
